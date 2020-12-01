22da4c9dadd1bbcf5cd142a28a7ed7e7ded13eb6
赵欢 <wb-zh358561@antfin.com>
2020-12-02 00:31:28 +0800

    feat: next pages modified

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,24 @@
-export default () => <span>Hello World</span>
+import Link from 'next/link';
+import Router from 'next/router';
+import { Button } from 'antd';
+import './index.css';
+
+export default () => {
+  const gotoTestB = () => {
+    Router.push({
+      pathname: '/b',
+      query: {
+        id: 2,
+      }
+    });
+  }
+
+  return (
+    <>
+      <Link href="/a?id=1" title="Hello">
+        <Button>Hello World</Button>
+      </Link>
+      <Button onClick={gotoTestB}>goToB</Button>
+    </>
+  )
+};
