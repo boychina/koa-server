9c1c5ef567da4f35bdb5e07231c9e020a4f22918
boychina <boychina@outlook.com>
2020-12-06 23:45:02 +0800

    feat: input modified

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const webpack = require('webpack')
-const withCss = require('@zeit/next-css')
-const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
-const config = require('./config')
+const webpack = require("webpack");
+const withCss = require("@zeit/next-css");
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+const config = require("./config");
 
 const configs = {
   // 编译文件的输出目录
-  distDir: 'dest',
+  distDir: "dest",
   // 是否给每个路由生成Etag
   generateEtags: true,
   // 页面内容缓存配置
@@ -16,64 +16,64 @@ const configs = {
     pagesBufferLength: 2,
   },
   // 在pages目录下那种后缀的文件会被认为是页面
-  pageExtensions: ['jsx', 'js'],
+  pageExtensions: ["jsx", "js"],
   // 配置buildId
   generateBuildId: async () => {
     if (process.env.YOUR_BUILD_ID) {
-      return process.env.YOUR_BUILD_ID
+      return process.env.YOUR_BUILD_ID;
     }
 
     // 返回null使用默认的unique id
-    return null
+    return null;
   },
   // 手动修改webpack config
   webpack(config, options) {
-    return config
+    return config;
   },
   // 修改webpackDevMiddleware配置
-  webpackDevMiddleware: config => {
-    return config
+  webpackDevMiddleware: (config) => {
+    return config;
   },
   // 可以在页面上通过 procsess.env.customKey 获取 value
   env: {
-    customKey: 'value',
+    customKey: "value",
   },
   // 下面两个要通过 'next/config' 来读取
   // 只有在服务端渲染时才会获取的配置
   serverRuntimeConfig: {
-    mySecret: 'secret',
+    mySecret: "secret",
     secondSecret: process.env.SECOND_SECRET,
   },
   // 在服务端渲染和客户端渲染都可获取的配置
   publicRuntimeConfig: {
-    staticFolder: '/static',
+    staticFolder: "/static",
   },
-}
+};
 
-if (typeof require !== 'undefined') {
-  require.extensions['.css'] = file => {}
+if (typeof require !== "undefined") {
+  require.extensions[".css"] = (file) => {};
 }
 
 module.exports = withBundleAnalyzer(
   withCss({
     webpack(config) {
-      config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/))
-      return config
+      config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
+      return config;
     },
     publicRuntimeConfig: {
       GITHUB_OAUTH_URL: config.GITHUB_OAUTH_URL,
       OAUTH_URL: config.OAUTH_URL,
     },
-    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
     bundleAnalyzerConfig: {
       server: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/server.html',
+        analyzerMode: "static",
+        reportFilename: "../bundles/server.html",
       },
       browser: {
-        analyzerMode: 'static',
-        reportFilename: '../bundles/client.html',
+        analyzerMode: "static",
+        reportFilename: "../bundles/client.html",
       },
     },
-  }),
-)
+  })
+);
